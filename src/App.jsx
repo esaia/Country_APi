39bd2cd570ea545a6d896c59fc3fe55698fb9642
esaia <esaia@gmail.com>
2023-09-25 12:38:04 +0400

    create new file for selecting country

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import CountryInfo from "./components/CountryInfo";
 import CurrencyExchange from "./components/CurrencyExchange";
-import SelectMui from "./components/Selectmui";
+import SelectCountry from "./components/SelectCountry";
 
 function App() {
   return (
     <div className="m-2">
       <div className="max-w-6xl md:w-full p-5 border border-gray-300 m-auto ">
-        <SelectMui />
+        <SelectCountry />
         <CountryInfo />
         <CurrencyExchange />
       </div>
